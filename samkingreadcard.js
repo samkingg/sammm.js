b12f2fd47b9b7d3d0f408286cd07bae1ae42be1f
samkingg <ma.almoraisi@gmail.com>
2017-02-08 19:07:40 +0300

    Update samkingreadcard.js

--- samkingreadcard.js
+++ samkingreadcard.js
@@ -1,7 +1,30 @@
 //document.querySelectorAll('[name="dfwidgetshorttextfield_1148578"]')[0].value= 'adreddds';
 
 
-function copyToClipboard(text) {
+function copyToClipboardsam(text) {
+//toastr.info(messages.generating_link);
+var fam = document.getElementById('complianceSelectTwo').value;
+var adres1 = document.querySelectorAll('[name="addressLine3"]')[0].value;
+var dateex = document.getElementById('primaryExpiryDate').value;
+var dateiss = document.getElementById('primaryIssueDate').value;
+var birthc = document.querySelectorAll('[name="complianceCountryOfBirth"]')[0].value;
+var birthd = document.getElementById('receiverDateOfBirth').value;
+var cardnum = document.getElementById('primaryInfoOne').value;
+var cintiss = document.getElementById('primaryIdCountryOfIssue').value;
+var cardtyp = document.querySelectorAll('[name="primaryId"]')[0].value;
+//var phone = document.querySelectorAll('[name="addressLine1"]')[0].value;
+var phone = document.getElementById('phoneNumber').value;
+// document.querySelectorAll('[name="mobileNumber"]')[0].value;
+var namerec = document.getElementById("receiverNameProxy").innerText;
+var part1 =('#6#')+(birthd) + ('#6#')+('#7#')+(cardnum) + ('#7#')+('#8#')+(cintiss) + ('#8#')+('#9#')+(cardtyp)+ ('#9#')+('#10#')+(phone)+('#10#')+('#11#')+(namerec)+('#11#');
+var allkl =(('readsamcards')+(fam) + ('#1#')+('#2#')+(adres1) + ('#2#')+('#3#')+(dateex) + ('#3#')+('#4#')+(dateiss) + ('#4#')+('#5#')+(birthc) + ('#5#') + (part1));
+
+
+ var result = copyToClipboard(allkl);
+ 
+ 
+ //var result = copyToClipboard(smid);
+console.log("copied?", result);
 
     if (window.clipboardData && window.clipboardData.setData) {
         // IE specific code path to prevent textarea being shown while dialog is visible.
@@ -27,27 +50,5 @@ function copyToClipboard(text) {
 	//copyToClipboard(document.getElementById('complianceSelectTwo').value);
 
 
-//toastr.info(messages.generating_link);
-var fam = document.getElementById('complianceSelectTwo').value;
-var adres1 = document.querySelectorAll('[name="addressLine3"]')[0].value;
-var dateex = document.getElementById('primaryExpiryDate').value;
-var dateiss = document.getElementById('primaryIssueDate').value;
-var birthc = document.querySelectorAll('[name="complianceCountryOfBirth"]')[0].value;
-var birthd = document.getElementById('receiverDateOfBirth').value;
-var cardnum = document.getElementById('primaryInfoOne').value;
-var cintiss = document.getElementById('primaryIdCountryOfIssue').value;
-var cardtyp = document.querySelectorAll('[name="primaryId"]')[0].value;
-//var phone = document.querySelectorAll('[name="addressLine1"]')[0].value;
-var phone = document.getElementById('phoneNumber').value;
-// document.querySelectorAll('[name="mobileNumber"]')[0].value;
-var namerec = document.getElementById("receiverNameProxy").innerText;
-var part1 =('#6#')+(birthd) + ('#6#')+('#7#')+(cardnum) + ('#7#')+('#8#')+(cintiss) + ('#8#')+('#9#')+(cardtyp)+ ('#9#')+('#10#')+(phone)+('#10#')+('#11#')+(namerec)+('#11#');
-var allkl =(('readsamcards')+(fam) + ('#1#')+('#2#')+(adres1) + ('#2#')+('#3#')+(dateex) + ('#3#')+('#4#')+(dateiss) + ('#4#')+('#5#')+(birthc) + ('#5#') + (part1));
 
-
- var result = copyToClipboard(allkl);
- 
- 
- //var result = copyToClipboard(smid);
-console.log("copied?", result);
 
